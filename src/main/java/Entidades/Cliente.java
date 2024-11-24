b948532fdeb6b6036079d0b7ebf41848d16f3eca
ErosMariotti <145143267+ErosMariotti@users.noreply.github.com>
2024-11-24 13:58:58 -0300

    update

--- src/main/java/Entidades/Cliente.java
+++ src/main/java/Entidades/Cliente.java
@@ -1,24 +1,39 @@
 package Entidades;
 
+import lombok.AccessLevel;
 import lombok.*;
 
 import javax.persistence.*;
 import java.io.Serializable;
-@Entity
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
 @Builder
-@ToString
+@Entity
 public class Cliente implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Setter(AccessLevel.NONE)
     private Long id;
+    @Column(name = "dni", unique = true)
+    private int dni;
+    private String apellido;
+    private String nombre;
 
-    @OneToMany(cascade = CascadeType.ALL)
-    // La columna que contiene la clave foránea de domicilio
-    // Se crea el FK en la tabla cliente
-    @JoinColumn(name = "fk_cliente")
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "fk_domicilio")
     private Domicilio domicilio;
+
+    @OneToMany(mappedBy = "cliente")
+    private List<Factura> facturas =  new ArrayList<Factura>();
+    @Builder
+    public Cliente(Long id, int dni, String apellido, String nombre, Domicilio domicilio, List<Factura> facturas) {
+        this.id = id;
+        this.dni = dni;
+        this.apellido = apellido;
+        this.nombre = nombre;
+        this.domicilio = domicilio;
+        this.facturas = (facturas != null) ? facturas : new ArrayList<>();
+    }
 }
--- src/main/java/Entidades/Domicilio.java
+++ src/main/java/Entidades/Domicilio.java
@@ -1,4 +1,19 @@
 package Entidades;
+import lombok.*;
 
-public class Domicilio implements java.io.Serializable {
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Data
+@Builder
+@Entity
+public class Domicilio implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nombreCalle;
+    private int numero;
+
+    @OneToOne(mappedBy = "domicilio")
+    private Cliente cliente;
 }
